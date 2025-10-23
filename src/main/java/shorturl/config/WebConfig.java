f867504be92a41168febf65ddd0ef91fa976ce34
Chesny <granadosvictor01@gmail.com>
2025-10-23 13:44:17 +0200

    Commit 4

--- src/main/java/shorturl/config/WebConfig.java
+++ src/main/java/shorturl/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://victorgranados.com")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
