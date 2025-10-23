9e08bde6ace86ec82367372945733347e64aea06
Chesny <granadosvictor01@gmail.com>
2025-10-23 16:13:28 +0200

    Commit 6

--- src/main/java/shorturl/infrastructure/entity/ShortUrlEntity.java
+++ src/main/java/shorturl/infrastructure/entity/ShortUrlEntity.java
@@ -1,8 +1,8 @@
 package shorturl.infrastructure.entity;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,12 +11,11 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+@Table(name = "short_url_entity") // 👈 Nombre real de la tabla en tu BD
 public class ShortUrlEntity {
     @Id
     private String id;
     private String originalUrl;
     private LocalDateTime createdAt;
     private int visits;
-
-
 }
